47096a8eabff83549f4bb2eaa6a2a4d74487f485
Daisuke Taniwaki <daisuketaniwaki@gmail.com>
2021-10-04 22:11:35 +0900

    Update for v0.1.4

--- git-kustomize-diff.rb
+++ git-kustomize-diff.rb
@@ -2,10 +2,10 @@ class GitKustomizeDiff < Formula
   desc "Diff tool for Kustomize"
   homepage "https://github.com/dtaniwaki/git-kustomize-diff"
   baseurl = "https://github.com/dtaniwaki/git-kustomize-diff/releases/download"
-  version "v0.1.1"
+  version "v0.1.4"
   archive_name = "git-kustomize-diff_darwin_x86_64.tar.gz"
   url baseurl + "/" + version + "/" + archive_name
-  sha256 "b54fcddb9935887194b2d7270186e45198a56f01acc089dc50af402caa9a7436"
+  sha256 "aea9dc08c73e8f38add052df590f1a9a063f2c7204ee508ccda22d9c099ce619"
   license "Apache-2.0"
   bottle :unneeded
   depends_on "git"
